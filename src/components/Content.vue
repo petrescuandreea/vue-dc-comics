5cbc0303df06c1bb76cd3ab482acf63c5ed8b953
Andreea1696 <andreeapetrescu16@gmail.com>
2021-11-24 16:15:34 +0100

    creato componente

---
+++ src/components/Content.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="container-center">
+       --> Content goes here 
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Content'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+    .container-center {
+        width: 75%;
+        margin: 0 auto;
+        color: white;
+        font-size: 30px;
+    }
+
+</style>
