4ca75fa85e7a542c32b9ef805c7a300248c188cd
Andreea1696 <andreeapetrescu16@gmail.com>
2021-11-25 17:06:16 +0100

    creato componente

---
+++ src/components/Jumbo.vue
@@ -0,0 +1,23 @@
+<template>
+    <section>
+
+    </section>
+</template>
+
+<script>
+export default {
+  name: 'Jumbo',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+    section {
+        height: 400px;
+        background-image: url('../assets/img/jumbotron.jpg');
+        background-size: cover;
+        background-repeat: no-repeat;
+        background-position: top;
+    }
+
+</style>
